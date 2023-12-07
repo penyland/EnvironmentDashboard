949b15dd92887672925aea145547ebbb3d2e925c
Peter Nylander <peter.nylander@gmail.com>
2023-12-07 13:45:30 +0100

    Bootstrapping repo

---
+++ src/EnvironmentDashboard/Features/Environments/Environment.cs
@@ -0,0 +1,24 @@
+﻿namespace EnvironmentDashboard.Features.Environments;
+
+public record Environment
+{
+    public Environment(long id, string name, string? url = null, string? description = null)
+    {
+        Id = id;
+        Name = name;
+        Url = url;
+        Description = description;
+    }
+
+    public long Id { get; init; }
+
+    public string Name { get; init; }
+
+    public string? Url { get; init; }
+
+    public string? Description { get; init; }
+
+    public List<Application> Applications { get; init; } = new();
+}
+
+public record Application(string Name, string Version, string OS);
